7f1171dc4ae2ee08de584d7df5f224ecfe7fd2e0
berghach <berghach.chemseddine@gmail.com>
2024-09-20 15:21:21 +0100

    BAT-4 updated the database connection code and added the DBConnectionTest

---
+++ src/database/DBConnection.java
@@ -0,0 +1,16 @@
+package database; /**
+ * PostgreSQLConnection
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DBConnection {
+    private static final String url = "jdbc:postgresql://localhost:5432/baticuisine";
+    private static final String user = "postgres";
+    private static final String password = "post2001";
+
+    public static Connection getConnection() throws SQLException
+    {
+        return DriverManager.getConnection(url, user, password);
+    }
+}
